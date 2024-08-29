c87956eaf6d42864ca43df6f9d033d7dc55ac357
yhfone <2502238189@qq.com>
2024-08-29 15:48:19 +0800

    first commit

---
+++ realsense2_camera/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export ROSLISP_PACKAGE_DIRECTORIES='/home/pi/legged_elevation_ws/src/realsense-ros/realsense2_camera/build/devel/share/common-lisp'
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/pi/legged_elevation_ws/src/realsense-ros/realsense2_camera/build/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/pi/.local/bin:/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/pi/legged_elevation_ws/src/realsense-ros/realsense2_camera/build'
+export ROS_PACKAGE_PATH="/home/pi/legged_elevation_ws/src/realsense-ros/realsense2_camera:$ROS_PACKAGE_PATH"
